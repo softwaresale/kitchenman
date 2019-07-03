4da3cf57171a96f39a675e334e54bc819d8f4560
softwaresale <softwaresale01@gmail.com>
2019-07-03 16:13:30 -0400

    frontend: can now add items to ingredient table

--- kitchenman-frontend/src/app/recipe/ingredients-edit/ingredients-edit.component.ts
+++ kitchenman-frontend/src/app/recipe/ingredients-edit/ingredients-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from 'src/app/recipe';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import IngredientDataSource from './ingredients.data-source';
-import { of } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-ingredients-edit',
@@ -12,9 +11,8 @@ import { of } from 'rxjs';
 export class IngredientsEditComponent implements OnInit {
 
   @Input() ingredients: Ingredient[];
-  ingredientsData: IngredientDataSource;
   columnsToDisplay = ['name', 'qty', 'unit', 'description'];
-
+  ingredientData: MatTableDataSource<Ingredient>;
   newIngredientForm: FormGroup;
 
   constructor(private fb: FormBuilder) { }
@@ -28,12 +26,16 @@ export class IngredientsEditComponent implements OnInit {
     });
 
     if (this.ingredients) {
-      this.ingredientsData = new IngredientDataSource(of(this.ingredients));
+      this.ingredientData = new MatTableDataSource<Ingredient>(this.ingredients);
+    } else {
+      console.error('ingredients are empty');
     }
   }
 
   onAddIngredient(): void {
+    // TODO add validation
     this.ingredients.push(this.newIngredientForm.value);
+    this.ingredientData.data = this.ingredients;
+    this.newIngredientForm.reset();
   }
-
 }
