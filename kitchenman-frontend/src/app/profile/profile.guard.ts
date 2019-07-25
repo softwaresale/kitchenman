09e82a35d006246395986947de8a7c0cc7dad684
softwaresale <softwaresale01@gmail.com>
2019-07-25 16:18:29 -0400

    frontend: integrated auth guard into recipe endpoints

--- kitchenman-frontend/src/app/profile/profile.guard.ts
+++ kitchenman-frontend/src/app/profile/profile.guard.ts
@@ -2,22 +2,29 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SessionService } from '../auth/session.service';
+import { Store, select } from '@ngrx/store';
+import { AppState, selectLoggedIn } from '../state/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileGuard implements CanActivate {
 
+  private loggedIn = false;
+
   constructor(
     private sessionService: SessionService,
     private router: Router,
-  ) { }
+    private store: Store<AppState>,
+  ) {
+    this.store.pipe(select(selectLoggedIn)).subscribe(status => this.loggedIn = status);
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if (this.sessionService.loggedIn) {
+    if (this.loggedIn) {
       return true;
     } else {
       console.log('You cannot go to profile. You are not logged in...');
